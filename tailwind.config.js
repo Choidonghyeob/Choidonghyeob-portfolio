e50278459ea0259f558a70d61c87685c62ab2fdf
Choidonghyeob <141224660+Choidonghyeob@users.noreply.github.com>
2024-01-09 12:53:26 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
         /**
          * @description 자기소개의 이름 부분, code tag의 darkmode에 사용되는 컬러입니다.
          */
-        PRIMARY: "#0000FF",
+        PRIMARY: "#C0C0C0",
 
         /**
          * @description code tag, link hover icon에 사용되는 컬러입니다.
