c50386668ee97016cd93635958d58e2d5ef71a21
Choidonghyeob <141224660+Choidonghyeob@users.noreply.github.com>
2024-01-09 12:49:42 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,17 +18,17 @@ module.exports = {
         /**
          * @description selection(드래그 블록)에 사용되는 컬러입니다.
          */
-        PRIMARY_LIGHT: "#00E3EC",
+        PRIMARY_LIGHT: "#D1D3D4",
 
         /**
          * @description 자기소개의 이름 부분, code tag의 darkmode에 사용되는 컬러입니다.
          */
-        PRIMARY: "#D1D3D4",
+        PRIMARY: "#0000FF",
 
         /**
          * @description code tag, link hover icon에 사용되는 컬러입니다.
          */
-        PRIMARY_HEAVY: "#087f5b",
+        PRIMARY_HEAVY: "#C0C0C0",
 
         /**
          * @description 페이지 최상단 gradient의 시작 컬러입니다.
