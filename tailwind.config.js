8beb30d4d7496ebdebf30594bb072c4a93631c72
Choidonghyeob <141224660+Choidonghyeob@users.noreply.github.com>
2024-01-09 12:58:48 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         /**
          * @description selection(드래그 블록)에 사용되는 컬러입니다.
          */
-        PRIMARY_LIGHT: "#212529",
+        PRIMARY_LIGHT: "#C0C0C0",
 
         /**
          * @description 자기소개의 이름 부분, code tag의 darkmode에 사용되는 컬러입니다.
